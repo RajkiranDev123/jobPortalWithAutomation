b2399d1ea523ccf4051a453ddab0ac35f4ea0980
raj <rajtech645@gmail.com>
2025-07-19 12:36:00 +0530

    db

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,25 @@
 import { config } from "dotenv"
-config({path:"./.env"})
+config({ path: "./.env" })
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import { connection } from "./database/connection.js";
 
 const app = express()
 
+app.use(
+    cors({
+        // origin: [process.env.FRONTEND_URL],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        // credentials: true,
+    })
+);
+
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connection();
 
 export default app
