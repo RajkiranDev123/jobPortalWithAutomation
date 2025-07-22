2dd22c7f114fe3635486145054d0da40cbef07b1
raj <rajtech645@gmail.com>
2025-07-22 09:57:50 +0530

    router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from 'react'
+import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const App = () => {
   return (
-    <>
-     
-  <p>hiiiiiiiiiiii</p>
-    </>
+    <div>App iougfghjkl</div>
   )
 }
 
