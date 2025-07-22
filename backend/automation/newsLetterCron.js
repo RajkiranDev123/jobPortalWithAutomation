a414d61fbf4cd2712a8a93c2ba789c6dcc7d4206
raj <rajtech645@gmail.com>
2025-07-22 09:15:19 +0530

    automation

--- backend/automation/newsLetterCron.js
+++ backend/automation/newsLetterCron.js
@@ -1,4 +1,13 @@
 import cron from "node-cron";
 import { Job } from "../models/jobSchema.js";
-import { User } from "../models/userSchema.js";
+import { UserModel } from "../models/userSchema.js";
 import { sendEmail } from "../utils/sendEmail.js";
+
+
+export const newsLetterCron = () => {
+    // min hour day-of-month month day-of-week
+    cron.schedule("* * * * *", async () => {
+        const jobs = await Job.find({ newsLettersSent: false });
+        console.log(555)
+    })
+}
