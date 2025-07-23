5a6c2eb997cd538eda24ff471f578cf59767cb24
raj <rajtech645@gmail.com>
2025-07-23 18:14:50 +0530

    slice for application

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,14 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import jobReducer from "./slices/jobSlice";
 import userReducer from "./slices/userSlice";
-// import applicationReducer from "./slices/applicationSlice";
+import applicationReducer from "./slices/applicationSlice";
 // import updateProfileReducer from "./slices/updateProfileSlice";
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     jobs: jobReducer,
-    // applications: applicationReducer,
+    applications: applicationReducer,
     // updateProfile: updateProfileReducer
   },
 });
