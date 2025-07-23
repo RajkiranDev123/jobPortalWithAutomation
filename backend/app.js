d8b35f76d2e7dcbb7c10208a0cfff100ae848f57
raj <rajtech645@gmail.com>
2025-07-23 10:43:20 +0530

    qr code

--- backend/app.js
+++ backend/app.js
@@ -14,9 +14,9 @@ const app = express()
 
 app.use(
     cors({
-        // origin: [process.env.FRONTEND_URL],
+        origin: [process.env.FRONTEND_URL],
         methods: ["GET", "POST", "PUT", "DELETE"],
-        // credentials: true,
+        credentials: true,
     })
 );
 
@@ -32,7 +32,7 @@ app.use(
     })
 );
 
-app.use("/api/v1/user",userRouter)
+app.use("/api/v1/user", userRouter)
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicationRouter);
 
