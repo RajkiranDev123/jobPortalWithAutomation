fad11aad034b9e20c36751164c5472e2f96ff4c4
raj <rajtech645@gmail.com>
2025-07-24 10:43:54 +0530

    bug fix

--- backend/routes/jobRouter.js
+++ backend/routes/jobRouter.js
@@ -10,5 +10,5 @@ router.get("/getall", getAllJobs);
 router.get("/getmyjobs", isAuthenticated, isAuthorized("Employer"), getMyJobs);
 router.delete("/delete/:id", isAuthenticated, isAuthorized("Employer"), deleteJob);
 
-router.get("/get/:id", getSingleJob)
+router.get("/get/:id",isAuthenticated, getSingleJob)
 export default router;
