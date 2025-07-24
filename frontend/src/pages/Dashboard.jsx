c5770627b979e88ac37b956d37395b420dce3f83
raj <rajtech645@gmail.com>
2025-07-24 10:44:12 +0530

    dashboard compononets setup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -17,9 +17,7 @@ const Dashboard = () => {
   const [show, setShow] = useState(false);
   const [componentName, setComponentName] = useState("My Profile");
 
-  const { loading, isAuthenticated, error, user } = useSelector(
-    (state) => state.user
-  );
+  const { loading, isAuthenticated, error, user } = useSelector((state) => state.user);
 
   const dispatch = useDispatch();
   const navigateTo = useNavigate();
