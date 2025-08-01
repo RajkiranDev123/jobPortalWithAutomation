d220ee583f096622c870f30b5357664bce8ddbe1
raj <rajtech645@gmail.com>
2025-08-01 17:56:45 +0530

    animation

--- frontend/src/components/TopNiches.jsx
+++ frontend/src/components/TopNiches.jsx
@@ -1,6 +1,7 @@
 import { BsDatabaseUp } from "react-icons/bs";
 import { MdOutlineAddToHomeScreen } from "react-icons/md";
 import { RiFlipVerticalLine } from "react-icons/ri";
+import ban1 from "../assets/ban1.png"
 const TopNiches = () => {
   const services = [
     {
@@ -30,11 +31,21 @@ const TopNiches = () => {
   ];
 
   return (
-    <section className="services">
-      <h3>Top Niches!</h3>
+    <section
+      style={{
+        backgroundImage: `url(${ban1})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+          opacity: 0.9, 
+      
+      }}
+
+      className="services">
+      <h3 style={{color:"white"}}>Top Niches!</h3>
       <div className="grid">
         {services.map(({ id, service, description, icon: Icon }) => (
-          <div className="card" style={{boxShadow: "rgba(72, 57, 236, 0.16) 0px 3px 6px, rgba(17, 72, 189, 0.23) 0px 3px 6px"}} key={id}>
+          <div className="card" style={{ boxShadow: "rgba(72, 57, 236, 0.16) 0px 3px 6px, rgba(17, 72, 189, 0.23) 0px 3px 6px" }} key={id}>
 
             <h4>{service}    {Icon && <Icon className="text-3xl text-blue-600 mb-2" />} </h4>
             <p>{description}</p>
