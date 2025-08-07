91eb30a9a18b4b31ed001de70dc7070b5328b44a
raj <rajtech645@gmail.com>
2025-08-07 13:05:22 +0530

    test cred

--- frontend/src/components/TestCredentials.jsx
+++ frontend/src/components/TestCredentials.jsx
@@ -1,34 +1,44 @@
 
+import { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 const TestCredentials = () => {
+    const [adminEmail, setAdminEmail] = useState(false)
+    const [adminPass, setAdminPass] = useState(false)
+
+    const [userEmail, setUserEmail] = useState(false)
+    const [userPass, setUserPass] = useState(false)
     return (
         <>
             {/*  */}
             <p style={{ fontSize: 14, fontFamily: "monospace", color: "grey", display: "flex", alignItems: "center", gap: 3 }}>Employer Test Email :
                 <CopyToClipboard text="rajtech645@gmail.com">
-                    <span style={{fontSize:14, cursor: "pointer", color: "blue", display: "flex", alignItems: "center"}}> Copy!</span>
+                    {!adminEmail ? <span onClick={() => setAdminEmail(!adminEmail)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copy!</span>
+                        : <span onClick={() => setAdminEmail(!adminEmail)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copied!</span>}
                 </CopyToClipboard>
             </p>
             <p style={{ fontSize: 14, fontFamily: "monospace", color: "grey", display: "flex", alignItems: "center", gap: 3 }}>Employer Test Password :
                 <CopyToClipboard text="12345678">
-                    <span style={{fontSize:14,  cursor: "pointer", color: "blue", display: "flex", alignItems: "center" }}> Copy!</span>
+                    {!adminPass ? <span onClick={() => setAdminPass(!adminPass)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copy!</span>
+                        : <span onClick={() => setAdminPass(!adminPass)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copied!</span>}
                 </CopyToClipboard>
             </p>
             {/*  */}
-            <p style={{color:"grey"}}>------------------</p>
+            <p style={{ color: "grey" }}>------------------</p>
 
             {/*  */}
             <p style={{ fontSize: 14, fontFamily: "monospace", color: "grey", display: "flex", alignItems: "center", gap: 3 }}>Job Seeker Test Email :
                 <CopyToClipboard text="rajkir783@gmail.com">
-                    <span style={{fontSize:14,  cursor: "pointer", color: "blue", display: "flex", alignItems: "center" }}> Copy!</span>
+                    {!userEmail ? <span onClick={() => setUserEmail(!userEmail)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copy!</span>
+                        : <span onClick={() => setUserEmail(!userEmail)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copied!</span>}
                 </CopyToClipboard>
             </p>
             <p style={{ fontSize: 14, fontFamily: "monospace", color: "grey", display: "flex", alignItems: "center", gap: 3 }}>Job Seeker Test Password :
                 <CopyToClipboard text="12345678">
-                    <span style={{fontSize:14,  cursor: "pointer", color: "blue", display: "flex", alignItems: "center", }}> Copy!</span>
+                    {!userPass ? <span onClick={() => setUserPass(!userPass)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copy!</span>
+                        : <span onClick={() => setUserPass(!userPass)} style={{fontSize:12, cursor: "pointer", color: "blue", display: "flex", alignItems: "center", textDecoration: "" }}> Copied!</span>}
                 </CopyToClipboard>
             </p>
-       
+
         </>
     )
 }
